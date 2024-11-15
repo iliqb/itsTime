ff9769475c7a4b05b1203434e3ee00f5bc24ecdf
iliqb <134982157+iliqb@users.noreply.github.com>
2024-11-14 21:59:00 -0600

    extention

---
+++ script.js
@@ -0,0 +1,8 @@
+function scheduleRemider() {
+    const title = document.getElementById("title")
+    const event = document.getElementById("event")
+    const date = document.getElementById("date")
+
+
+    }
+}
