93307b761dc05b538e8c38085576340f60c2f440
iliqb <134982157+iliqb@users.noreply.github.com>
2024-11-22 19:43:03 -0600

    delete fuction

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function scheduleRemider() {
-    const title = document.getElementById("title")
-    const event = document.getElementById("event")
-    const date = document.getElementById("date")
-
-
-    }
+document.addEventListener(DOMContentLoaded,() = >) {
+    
 }
+
